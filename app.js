3e8fae3fd2775b260a60c6b490824fbd57fca006
Paul Sicking <sickingp@gmail.com>
2025-03-08 13:07:32 -0800

    Added Jan

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! This is Paul'));
+app.get('/', (req, res) => res.send('Hello World! This is Paul and Jan'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
